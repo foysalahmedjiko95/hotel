6bdea5b079698381b8d3930fa0686ff67bb10a01
unknown <foysal.ahmed.jiko95@gmail.com>
2022-08-21 15:58:59 +0600

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Go BOoK </h1>
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
